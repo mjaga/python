345a10a4a5a53d754ebffeca8a99e1ecb3eba37a
jagadeeshmurugaiyan <itsjaga26@gmail.com>
2019-02-16 20:23:45 +0530

    sumofobject written in function to display

--- sumofobject.py
+++ sumofobject.py
@@ -1,7 +1,14 @@
-mainObj = {"Rick": 85, "Amit": 42, "George": 53, "Tanya": 60, "Linda": 35}
-print('Main object:',mainObj);
-addvalue = 0;
-for i in mainObj:
-    addvalue += mainObj[i];
+class SumOfObject:
+   def __init__(self, Obj):
+       self.mainObj = Obj
+   def display(self): 
+        print('Main object:',self.mainObj);
+        addvalue = 0;
+        for i in self.mainObj:
+           addvalue += self.mainObj[i];
     
-print('total value:',addvalue);
+        print('total value:',addvalue);
+
+
+outuput = SumOfObject({"Rick": 85, "Amit": 42, "George": 53, "Tanya": 60, "Linda": 35})
+outuput.display()    
